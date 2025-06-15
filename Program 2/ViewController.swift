0897f79bee2b5d5394bc5397fee32585b6da7f0f
swift <sixfold.54-county@icloud.com>
2025-06-15 19:56:20 +0100

    Initial Commit

---
+++ Program 2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Program 2
+//
+//  Created by swift on 15/06/2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
